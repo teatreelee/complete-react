7862d515927bf60f9f05b7001166104a98a657f5
Sang Theresa Lee <tsahlee@gmail.com>
2017-01-29 21:14:51 -0500

    createFactory

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -14,16 +14,27 @@ var MyTitle = React.createClass({
     }
 })
 
+
+//methods for cleaner code
+
+var MyTitleFactory = React.createFactory(MyTitle)
+ce = React.createElement
+
 //createElement is one specific instance of the class. Can be written multiple times.
 //Reason for encircling div: You can't have MyFirstComponent be equal to several different elements. It has to equal one thing. It can be one element with many chidren.
 //Analogy: We cannot have x = 1, 2, 3, 4,5. We can have x = [1,2,3,4,5]
 var MyFirstComponent = (
     div(null,
+        // MyTitleFactory, React.createElement(MyTitle, null), and ce(MyTitle, null) all achieve the same thing
+        MyTitleFactory(null),
         React.createElement(MyTitle, null),
         React.createElement(MyTitle, null),
-        React.createElement(MyTitle, null),
-        React.createElement(MyTitle, null)
+        ce(MyTitle, null)
     )
 );
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'));
+
+//What is the difference between a factory and an element?
+    // A factory makes this into a function that when called, returns the component.
+    // createElement calls MyTitle underneath the hood.
