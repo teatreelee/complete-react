f884d983cf8dd5beca6048bd1a2d10caad92c282
Sang Theresa Lee <tsahlee@gmail.com>
2017-01-29 21:37:24 -0500

    Props

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,13 +2,15 @@ var div = React.DOM.div;
 var h1 = React.DOM.h1;
 
 
-//creates a new type of element that can be used over again. We will only create this once
-//known as a composite component. It is a component of our own creation.
+//all components in React must have a render function, which must return a component.
+// if you comment out the return and check out the page, you will see:
+    //ReactCompositeComponent.render(): A valid React element (or null) must be returned. You may have returned undefined, an array or some other invalid object.
 var MyTitle = React.createClass({
     render() {
         return (
                 div (null,
-                     h1(null, 'Check this out!')
+                     //props: short for properties.
+                     h1(null, this.props.title)
                      )
                 )
     }
@@ -16,25 +18,17 @@ var MyTitle = React.createClass({
 
 
 //methods for cleaner code
-
 var MyTitleFactory = React.createFactory(MyTitle)
 ce = React.createElement
 
-//createElement is one specific instance of the class. Can be written multiple times.
-//Reason for encircling div: You can't have MyFirstComponent be equal to several different elements. It has to equal one thing. It can be one element with many chidren.
-//Analogy: We cannot have x = 1, 2, 3, 4,5. We can have x = [1,2,3,4,5]
+
 var MyFirstComponent = (
     div(null,
         // MyTitleFactory, React.createElement(MyTitle, null), and ce(MyTitle, null) all achieve the same thing
-        MyTitleFactory(null),
-        React.createElement(MyTitle, null),
-        React.createElement(MyTitle, null),
-        ce(MyTitle, null)
+        MyTitleFactory({title: 'Hello'}),
+        React.createElement(MyTitle, {title: 'World'}),
+        ce(MyTitle, {title: '!'})
     )
 );
 
 ReactDOM.render(MyFirstComponent, document.getElementById('app'));
-
-//What is the difference between a factory and an element?
-    // A factory makes this into a function that when called, returns the component.
-    // createElement calls MyTitle underneath the hood.
