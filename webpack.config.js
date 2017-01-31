ed1364fe81dff5cf62d8cad641b02419db5115f6
Sang Theresa Lee <tsahlee@gmail.com>
2017-01-31 09:15:13 -0500

    .gitignore

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname, //allows you to know where you're running this from and run from anywhee
+  entry: './js/ClientApp.js', //where we enter our webpack (mainfile)
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json'] //order for webpack to look for file extensions
+  },
+  stats: {
+    colors: true, //for command line
+    reasons: true, //for error messages
+    chunks: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/, //makes sure that babel only looks for js or jsx files and not ext like css
+        loader: 'babel-loader' //any with those file extensions should be run through this
+      }
+    ]
+  }
+
+}
